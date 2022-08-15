715a114aa63868dc703cbd2cc587cf00a33badbc
Yash Bansal <bansal_yash@outlook.com>
2022-08-15 20:47:29 +0530

    Added learn-spfx--wp-nojs-spo solution

---
+++ learn-spfx--wp-nojs-spo/src/webparts/noJsWebPart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface INoJsWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'NoJsWebPartWebPartStrings' {
+  const strings: INoJsWebPartWebPartStrings;
+  export = strings;
+}
